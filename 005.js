4f9fe0a4f749610cce22956e296c26eb5b9234ff
dishantraut <63045109+dishantraut@users.noreply.github.com>
2022-10-01 19:13:45 +0530

    Logarithms

---
+++ 005.js
@@ -0,0 +1,23 @@
+##Logarithms
+
+We've encountered some of the most common complexities: O(1), O(n), O(n^2)
+
+Sometimes big O expressions involve more complex mathematical expressions
+One that appears more often than you might like is the logarithm!
+
+
+The logarithm of a number roughly measures the number of times you can divide 
+that number by 2 before you get a value that's less than or equal to one.
+
+
+Logarithm Complexity
+Logarithmic time complexity is great!
+
+
+## Who Cares ?
+
+Certain searching algorithms have logarithmic time complexity.
+Efficient sorting algorithms involve logarithms.
+Recursion sometimes involves logarithmic space complexity.
+
+...and more!
