ffbae8075a0c7d55e041192bef5daf47ac9f3490
dishantraut <63045109+dishantraut@users.noreply.github.com>
2022-10-01 19:28:25 +0530

    Big O of Arrays

---
+++ 007.js
@@ -0,0 +1,13 @@
+/*
+* ARRAYS (Ordered lists)
+* WHEN TO USE ARRAYS
+1. When you need order
+2. When you need fast access / insertion and removal (sort of....)
+* Limitations of Arrays
+1. Inserting at the beginning is not as easy as we might think! 
+2. There are more efficient data structures for that!
+*/
+
+let names = ["Michael", "Melissa", "Andrea"];
+let values = [true, {}, [], 2, "awesome"];
+
